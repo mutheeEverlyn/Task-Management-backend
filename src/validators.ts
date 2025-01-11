0ece7f6488f134d0402564263612cc67093a2963
Everlyn Muthoni <muthee.14952@students.kyu.ac.ke>
2025-01-11 15:47:26 +0330

    new file

---
+++ src/validators.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const userSchema = z.object({
+    full_name: z.string(),
+    email: z.string(),
+    contact_phone: z.string(),
+    address:z.string(),
+    password: z.string(),
+    role: z.string(),
+    created_at:z.string().optional(),
+    updated_at:z.string().optional()
+})
+export const taskSchema=z.object({
+    task:z.string(),
+    user_id:z.number(),
+    created_at:z.string().optional(),
+    updated_at:z.string().optional()
+})
